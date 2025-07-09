39013d55470f095b9f0663555239870ea8f1b5ba
OleksandrHub <sasha.klishch1@gmail.com>
2025-07-09 17:57:03 +0300

    feat: add authentication module
    - Created login and registration components
    - Implemented basic styling for auth forms
    - Moved styles to separate files
    - Updated app.ts imports
    - Removed unused files and styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DashboardComponent } from './dashboard/components/dashboard/dashboard';
+import { LoginComponent } from './auth/components/login/login';
+import { RegistationComponent } from './auth/components/registration/registration';
 
 @Component({
   selector: 'app-root',
-  imports: [DashboardComponent],
+  imports: [DashboardComponent, LoginComponent, RegistationComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/auth/components/registration/registration.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registation',
+  imports: [],
+  templateUrl: './registration.html',
+  styleUrl: './registration.scss'
+})
+export class RegistationComponent {
+
+}
